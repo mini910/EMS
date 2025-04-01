07c7f0a25f8d85275068e35477414bf780511256
mini910 <minis4741@gmail.com>
2025-04-01 14:16:49 +0530

    Add files via upload

---
+++ utils/footer.php
@@ -0,0 +1,44 @@
+<hr class = "footerline"><!--css modified horizontal line-->
+<footer>
+    <div class = "container">
+        <div class = "row">
+            <section>
+                <div class = "footerContent col-md-4"><!--left content-->
+                    <p class = "footerContent1">
+                        <strong>K</strong><span class = "small footerSubtext">ingston</span>
+                        <strong>E</strong><span class = "small footerSubtext">ducational</span>
+                        <strong>I</strong><span class = "small footerSubtext">nstitute</span>
+                        
+                    </p>
+
+                    <p class = "footerSubtext2">
+                        Event Management System
+                        &copy; <br>
+                        Developed by Group1.
+                    </p>
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--middle content-->
+                Developed by: Rup Paul,<br>
+                Mini Singh,<br>
+                Trisha Chakraborty,<br>
+                Triyasha Banik,<br>
+                Shreya Aditya,<br>
+                Santanu Bhattacharya,<br>
+                Soumojit Das.
+
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--right content-->
+                    Follow Us:<br>
+                        <img src = "images/facebook.png">
+                        <img src = "images/twitter.png">
+                        <img src = "images/googleplus.png">
+                        <img src = "images/youtube.png">
+                </div>
+            </section>
+        </div>
+    </div>
+</footer>
